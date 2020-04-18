9579aed71d88731d35974f1e8331b442e34183f5
Mason Burke <masonburke@rocketmail.com>
2020-04-18 15:25:30 -0600

    employee name, id, email test passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,46 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor ( name, id, email ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    }
+
+    getName(){
+    console.log(`Name: ${this.name}`)
+    }
+
+    getId(){
+    console.log(`ID#: ${this.id}`)
+    }
+
+    getEmail(){
+    console.log(`Email: ${this.email}`)
+    }
+
+    getRole(){
+
+    }
+
+    }
+
+    const newEmployee = new Employee ("mason", 32, "masonburke@rocketmail.com")
+    newEmployee.getName()
+    newEmployee.getId()
+    newEmployee.getEmail()
+
+
+module.exports = Employee
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Employee;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = requireO("./Employee")
+
+class Intern extends Employee {
+    constructor (name, id, email, school){
+
+    }
+}
+
+// getSchool(){
+
+// }
+
+// getRole(){
+
+// }
